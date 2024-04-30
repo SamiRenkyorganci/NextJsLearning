6b4f5dec60e903046838d1b299f6600c9dab5e55
SamiRenkyorganci <samirenkyorganci@gmail.com>
2024-04-30 18:02:18 +0300

    add about page

---
+++ next-imdb/src/app/about/page.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className=" text-2xl font-medium text-amber-600 "> About </h1>
+      <p>
+        IMDb, or the Internet Movie Database, is the world’s most popular and
+        authoritative source for movie, TV, and celebrity content. Founded in
+        1990 by a passionate movie fan, our platform offers an extensive
+        database where you can find everything from blockbuster films to indie
+        hits. IMDb not only provides detailed information about films and TV
+        shows but also serves as a guide to what's new and popular in the
+        entertainment world. Whether you are looking to check the latest
+        ratings, watch trailers, or find showtimes, IMDb is your ultimate
+        entertainment resource.
+      </p>
+      <p>
+        Our mission is to illuminate the world of entertainment. With millions
+        of visitors each day, we strive to be the most trusted and engaging
+        entertainment resource. IMDb is more than just an entertainment
+        database; it's a community where fans can rate their favorite shows,
+        review movies, and discuss their deepest theories about upcoming plots.
+        Here, you can also follow your favorite celebrities to receive updates
+        on their latest projects and achievements.
+      </p>
+      <p>
+        Engaging with IMDb means becoming part of a vibrant community that
+        values entertainment and its impact on global culture. You can
+        contribute to our expansive database by adding reviews, updating
+        information, or suggesting new content. Stay connected with us through
+        our website and mobile apps to receive personalized recommendations and
+        keep up with the latest trends in entertainment. At IMDb, every voice
+        matters as we work together to create the most comprehensive and dynamic
+        entertainment experience on the internet.
+      </p>
+    </div>
+  );
+}
