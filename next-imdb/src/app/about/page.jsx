45c03c7a950e8da54b835cf2e205d52b79388cda
SamiRenkyorganci <samirenkyorganci@gmail.com>
2024-05-01 15:18:54 +0300

    bug fixed eslint

--- next-imdb/src/app/about/page.jsx
+++ next-imdb/src/app/about/page.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <h1 className=" text-2xl font-medium text-amber-600 "> About </h1>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry s standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
@@ -17,7 +17,7 @@ export default function About() {
       </p>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry s standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
@@ -28,7 +28,7 @@ export default function About() {
       </p>
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industry s standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
