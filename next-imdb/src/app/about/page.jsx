c8628fd2b25ed10918cee6ea56023dd84639b2d5
SamiRenkyorganci <samirenkyorganci@gmail.com>
2024-05-01 15:10:36 +0300

    bug fixed

--- next-imdb/src/app/about/page.jsx
+++ next-imdb/src/app/about/page.jsx
@@ -5,12 +5,12 @@ export default function About() {
     <div className="max-w-6xl mx-auto p-3 space-y-4">
       <h1 className=" text-2xl font-medium text-amber-600 "> About </h1>
       <p>
-        IMDb, or the Internet Movie Database, is the world’s most popular and
+        IMDb, or the Internet Movie Database, is the worlds most popular and
         authoritative source for movie, TV, and celebrity content. Founded in
         1990 by a passionate movie fan, our platform offers an extensive
         database where you can find everything from blockbuster films to indie
         hits. IMDb not only provides detailed information about films and TV
-        shows but also serves as a guide to what's new and popular in the
+        shows but also serves as a guide to what s new and popular in the
         entertainment world. Whether you are looking to check the latest
         ratings, watch trailers, or find showtimes, IMDb is your ultimate
         entertainment resource.
@@ -19,7 +19,7 @@ export default function About() {
         Our mission is to illuminate the world of entertainment. With millions
         of visitors each day, we strive to be the most trusted and engaging
         entertainment resource. IMDb is more than just an entertainment
-        database; it's a community where fans can rate their favorite shows,
+        database; it s a community where fans can rate their favorite shows,
         review movies, and discuss their deepest theories about upcoming plots.
         Here, you can also follow your favorite celebrities to receive updates
         on their latest projects and achievements.
